a566994a925db44e1436996bdb6a0a455dfabe93
senuravihanjayadeva <senurajayadeva@gmail.com>
2021-11-25 18:51:18 +0530

    Implemented Event Route

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -3,10 +3,21 @@ import controller from "../controllers";
 import middleware from "../middleware";
 
 export default function (app: Express) {
+  /**
+   * @todo implement the @routes for UserController
+   */
   app.post("/user/", controller.createUser);
+
   /**
    * @todo implement the @routes for EventController
    */
+  app.post("/event/", controller.insertEvent);
+  app.get("/event/", controller.getEvents);
+  app.get("/event/:eventId/", controller.getEvent);
+  app.get("/pastevent/", controller.getPastEvents);
+  app.get("/upcomingevent/", controller.getUpcomingEvent);
+  app.put("/event/:eventId", controller.updateEvent);
+  app.delete("/event/:eventId", controller.deleteEvent);
   /**
    * @todo  implement the @routes for WebinarController
    */
