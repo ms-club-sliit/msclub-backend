534135e6450d2d687ab66656f0400f42f32ef309
senuravihanjayadeva <senurajayadeva@gmail.com>
2021-11-25 14:46:52 +0530

    Add todos for routes

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,7 +1,22 @@
-import { Express } from 'express';
-import controller from '../controllers';
-import middleware from '../middleware';
+import { Express } from "express";
+import controller from "../controllers";
+import middleware from "../middleware";
 
 export default function (app: Express) {
-  app.post('/user/', controller.createUser);
+  app.post("/user/", controller.createUser);
+  /**
+   * @todo implement the @routes for PastEventController
+   */
+  /**
+   * @todo  implement the @routes for UpcomingEventController
+   */
+  /**
+   * @todo  implement the @routes for PastWebinarController
+   */
+  /**
+   * @todo  implement the @routes for UpcomingWebinarController
+   */
+  /**
+   * @todo  implement the @routes for TopSpeakerController
+   */
 }
