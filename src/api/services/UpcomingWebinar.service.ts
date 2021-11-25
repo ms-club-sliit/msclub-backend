e81c85888f2329f415ed7c267c6b2a97570e72df
senuravihanjayadeva <senurajayadeva@gmail.com>
2021-11-25 14:45:49 +0530

    Add todos for services

---
+++ src/api/services/UpcomingWebinar.service.ts
@@ -0,0 +1,19 @@
+/**
+ * @todo create @function insertUpcomingWebinar to save a Upcoming Webinar in the database
+ */
+
+/**
+ * @todo create @function getUpcomingWebinar to fetch a Upcoming Webinar in the system
+ * @param upcomingWebinarId @type string
+ */
+
+/**
+ * @todo create @function updateUpcomingWebinar to update a Upcoming Webinar in the system
+ * @param upcomingWebinarId @type string
+ * @param updateData @type DocumentDefinition<IWebinar>
+ */
+
+/**
+ * @todo create @function deleteUpcomingWebinar to delete a Upcoming Webinar
+ * @param upcomingWebinarId @type string
+ */
--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,5 +1,35 @@
-import { insertUser } from './User.service';
+import { insertUser } from "./User.service";
+/**
+ * @todo import @methods from PastEvent Service
+ */
+/**
+ * @todo import @methods from UpcomingEvent Service
+ */
+/**
+ * @todo import @methods from PastWebinar Service
+ */
+/**
+ * @todo import @methods from UpcomingWebinar Service
+ */
+/**
+ * @todo import @methods from TopSpeaker Service
+ */
 
 export default {
   insertUser,
-}
+  /**
+   * @todo export @methods from PastEvent Service
+   */
+  /**
+   * @todo export @methods from UpcomingEvent Service
+   */
+  /**
+   * @todo export @methods from PastWebinar Service
+   */
+  /**
+   * @todo export @methods from UpcomingWebinar Service
+   */
+  /**
+   * @todo export @methods from TopSpeaker Service
+   */
+};
