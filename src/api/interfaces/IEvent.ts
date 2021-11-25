703175ac65f0c225bd1d3ba6f2749643837fd3cb
senuravihanjayadeva <senurajayadeva@gmail.com>
2021-11-25 18:50:39 +0530

    implemented event interface

--- src/api/interfaces/IEvent.ts
+++ src/api/interfaces/IEvent.ts
@@ -1,3 +1,14 @@
-/**
- * @todo implement the @interface IEvent 
- */
+import { Document } from 'mongoose';
+
+interface IEvent extends Document {
+  eventName : string;
+  description : string;
+  imageUrl : string;
+  dateTime : Date;
+  tags ?: string[];
+  link ?: string;
+  eventType : string;
+  isDeleted : boolean;
+};
+
+export type { IEvent };
