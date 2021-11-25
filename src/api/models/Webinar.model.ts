016859a3e8e462546688d4e6e239da4bad93ef47
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2021-11-25 23:59:21 +0530

    implemented the Webinar model

--- src/api/models/Webinar.model.ts
+++ src/api/models/Webinar.model.ts
@@ -1,3 +1,20 @@
-/**
- * @todo implement the @model Webinar 
- */
+import mongoose, { Schema } from "mongoose";
+import { IWebinar } from "../interfaces/IWebinar";
+
+const WebinarSchema = new Schema<IWebinar>(
+    {
+        webinarName: { type: String, required: true },
+        description: { type: String, required: true },
+        imageURL: { type: String, required: true },
+        dateTime: { type: Date, required: true },
+        time: { type: Date, required: true },
+        tags: [{ type: String, required: false }],
+        link: { type: String, required: true },
+        isDeleted: { type: Boolean, required: true },
+    },
+    { timestamps: true }
+);
+
+const WebinarModel = mongoose.model<IWebinar>("webinar", WebinarSchema);
+
+export default WebinarModel;
