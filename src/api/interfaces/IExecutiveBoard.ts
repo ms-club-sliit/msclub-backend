8aebc52f3f4f87d976d18eced097f05b78c96b16
rusiruavb <rusiruavb98@gmail.com>
2021-11-26 12:33:36 +0530

    Revert "added todos for excecutive board"
    This reverts commit a89a5bb69b2652ae3948ce188c8cac943ed0dd83.

--- src/api/interfaces/IExecutiveBoard.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-import { IBoardMember } from './IBoardMember';
-
-interface IExecutiveBoard extends Document {
-    serviceTerm: string;
-    boardMember: IBoardMember[];
-}
-export type { IExecutiveBoard };
