b18a8bc255351399f79922fd7b1b09d3163b41d2
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2021-11-28 01:21:06 +0530

    Application Service_Bckend-Part

--- src/api/services/Application.service.ts
+++ src/api/services/Application.service.ts
@@ -1,17 +1,58 @@
+import { DocumentDefinition, FilterQuery } from "mongoose";
+import { IApplication} from "../interfaces";
+import ApplicationModel from "../models/Application.model";
+
+
 /**
  * @todo create @function addApplication to save an Application in the database
  */
+export const addApplication = async (applicationData: DocumentDefinition<IApplication>) => {
+  return await ApplicationModel.create(applicationData)
+    .then(async (application) => {
+      return application;
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+};
 
 /**
  * @todo create @function fetchApplicationById to fetch a Application  in the system
  * @param applicationId @type string
  */
+export const fetchApplicationById = async (applicationId: string) => { 
+    return await ApplicationModel.findById(applicationId)
+    .then(async (application) => {
+        return application;
+    })
+    .catch((error) => {
+        throw new Error(error.message);
+    });
+};
 
 /**
  * @todo create @function fetchApplications to fetch all the Applications in the system
  */
+export const fetchApplications = async () => {
+    return await ApplicationModel.find()
+    .then(async (applications) => {
+        return applications;
+    })
+    .catch((error) => {
+        throw new Error(error.message);
+    });
+};
 
 /**
  * @todo create @function archiveApplication to delete an Application
  * @param applicationId @type string
  */
+export const archiveApplication = async (applicationId: string) => {
+    return await ApplicationModel.findByIdAndDelete(applicationId)
+    .then(async (application) => {
+        return application;
+    })
+    .catch((error) => {
+        throw new Error(error.message);
+    });
+};
