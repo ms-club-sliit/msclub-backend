0a3b3054f4167780e97ab380b3f34962efc87ea9
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2021-12-11 15:03:59 +0530

    Implemented User Service methods

--- src/api/services/User.service.ts
+++ src/api/services/User.service.ts
@@ -19,18 +19,88 @@ export const insertUser = async (userData: DocumentDefinition<IUser>) => {
 }
 
 /**
- * @todo create @function getUsers to fetch all the users in the system
+ *get all users
  */
-
+ export const getUsers = async () => {
+  return await UserModel.find()
+    .then((users) => {
+      return users;
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+};
 
 /**
- * @todo create @function updateUser to update a user in the system
+ * update user
  * @param userId @type string
  * @param updateData @type DocumentDefinition<IUser>
  */
-
+export const updateData = async (
+  userId: string,
+  updateData: DocumentDefinition<IUser>
+) => {
+  return await UserModel.findById(userId)
+    .then(async (userDetails) => {
+      if (userDetails) {
+        if (userDetails.deletedAt) {
+          if (updateData.firstName) {
+            userDetails.firstName = updateData.firstName;
+          }
+          if (updateData.lastName) {
+            userDetails.lastName = updateData.lastName;
+          }
+          if (updateData.phoneNumber01) {
+            userDetails.phoneNumber01 = updateData.phoneNumber01;
+          }
+          if (updateData.phoneNumber02) {
+            userDetails.phoneNumber02 = updateData.phoneNumber02;
+          }
+          if (updateData.email) {
+            userDetails.email = updateData.email;
+          }
+          if (updateData.userName) {
+            userDetails.userName = updateData.userName;
+          }
+          if (updateData.password) {
+            userDetails.password = updateData.password;
+          }
+          if (updateData.profileImageUrl) {
+            userDetails.profileImageUrl = updateData.profileImageUrl;
+          }
+          if (updateData.type) {
+            userDetails.type = updateData.type;
+          }
+          
+          return await userDetails.save();
+        } else {
+          throw new Error("User is not found");
+        }
+      } else {
+        return null;
+      }
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+};
 
 /**
- * @todo create @function deleteUser to delete the user
+ * delete user
  * @param userId @type string
  */
+export const deleteUser = async (userId: string) => {
+  return await UserModel.findById(userId)
+    .then(async (userDetails) => {
+      if (userDetails?.deletedAt) {
+        userDetails.deletedAt = new Date();
+        return await userDetails.save();
+      } else {
+        return null;
+      }
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+};
+
