d79c51ed1d95d5f30fd784fcf5b80d56731e8b55
senuravihanjayadeva <senurajayadeva@gmail.com>
2021-12-31 10:38:04 +0530

    Create Test with Jest

---
+++ src/api/controllers/eventcontroller.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+describe('Test PingController',  () => {
+  it('Request /ping should return Pong!', async () => {
+    const result = await request(app).get('/event').send();
+    jest.useFakeTimers();
+    jest.spyOn(console, 'log');
+    jest.advanceTimersByTime(10000);
+    expect(result.status).toBe(201);
+  });
+});
