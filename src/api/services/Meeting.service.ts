d27cf3c16a72a5f374f285d830df4bc4d9af1d5b
Chathurika <Chathurika@LAPTOP-DGFGMJLI>
2022-03-16 19:22:58 +0530

    Make changes to MeetingService

--- src/api/services/Meeting.service.ts
+++ src/api/services/Meeting.service.ts
@@ -5,25 +5,24 @@ import { Request } from "express";
 import axios from "axios";
 
 export const scheduleInternalMeetingMSTeams = (request: Request, meetingData: DocumentDefinition<IMeeting>) => {
-	console.log(meetingData);
+	// console.log(meetingData);
 	return axios
 		.post(`${process.env.MS_MEETING_MANAGER_API}/api/msteams/internalmeeting/schedule`, meetingData)
 		.then(() => {
-			return "meeting";
-			// const meetingInfo = {
-			// 	meetingName: meeting.meetingName,
-			// 	startDateTime: meeting.startDateTime,
-			// 	endDateTime: meeting.endDateTime,
-			// 	emailList: [meeting.emailList],
-			// 	sheduledLink: sheduledLink,
-			// };
-			// MeetingModel.create(meetingInfo)
-			// 	.then((createdMeeting) => {
-			// 		return createdMeeting;
-			// 	})
-			// 	.catch((error) => {
-			// 		throw new Error(error.message);
-			// 	});
+			const meetingInfo = {
+				meetingName: meetingData.meetingName,
+				startDateTime: meetingData.startDateTime,
+				endDateTime: meetingData.endDateTime,
+				emailList: [meetingData.emailList],
+				sheduledLink: "sheduledLink",
+			};
+			MeetingModel.create(meetingInfo)
+				.then((createdMeeting) => {
+					return createdMeeting;
+				})
+				.catch((error) => {
+					throw new Error(error.message);
+				});
 		})
 		.catch((error) => {
 			throw new Error(error.message);
